1a680e5774c68773b8983be98861d443514c5ddf
Ramon Guimarães <ramon.cruz@ufu.br>
2022-03-31 12:32:00 -0300

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">YEZEY</a>
+      </header>
+      <main>
+        É OS BOOT
+      </main>
+    </div>
+  );
+}
+
+export default App;
